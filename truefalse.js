63293f7276523aead9a597eb15a5dbd65f6c89ec
ishtiaqahammed97 <76918709+ishtiaqahammed97@users.noreply.github.com>
2021-02-02 16:00:02 +0600

    exploring advance javaScript

---
+++ truefalse.js
@@ -0,0 +1,18 @@
+// const age = 0;
+
+// if(age>0){
+//     console.log("condition is true")
+// }
+// else{
+//     console.log("condition is false")
+// }
+
+// const name = " "; /** khali string thakle condition false, empty string thakle o, null thakle o ,Nan o, value jodi false set kore dei tahole o false dekhabe */
+// let name /**variable er value set na korle undefined dekhabe */
+let name= 0;
+if(name || name == 0){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
