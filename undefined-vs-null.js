c5e5c1241503270b361220a254e7317d618485e4
ishtiaqahammed97 <76918709+ishtiaqahammed97@users.noreply.github.com>
2021-02-02 20:46:02 +0600

    null vs undefined interview question

---
+++ undefined-vs-null.js
@@ -0,0 +1,18 @@
+// let pakhi; /** var set na kore dile */
+// function add(num1, num2){
+// console.log(num1 + num2);
+// return
+// } /** function e return na korle, return set na korle */
+
+// function add(num1, num2)
+// const result = add(13); /** parameter pass na korle */
+
+// const premik = { name :" smart dude", phone : 458743};
+// console.log(premik.gf) /** var emon kono jodi element na thake , ar shetake log kora hoy */
+
+// let fun = undefined; /** bandor type developer jodi undefined kore daw */
+
+
+
+// null  /** not existed */
+
